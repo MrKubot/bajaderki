bacb8a67d93901b88ae078a60b2a50aaa167f72e
RoundedHedgehog <82833875+RoundedHedgehog@users.noreply.github.com>
2022-05-09 23:21:01 +0200

    pozwolenie na pythonanywhere

--- bajaderki/settings.py
+++ bajaderki/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('BAJADERKI_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG_STATE')
 
-ALLOWED_HOSTS = ['bigbagofbajaderki.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['bigbagofbajaderki.herokuapp.com', 'roundedhedgehog.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
